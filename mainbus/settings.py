75cf9999b0b3b17560946e8e5c7221823946e35e
JadonZufall <jadonzufall@gmail.com>
2025-03-26 21:48:52 +0000

    Changed some setup tools to try and get this to work.

--- mainbus/settings.py
+++ mainbus/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xu3$gii%)#nj+#69lm*i5e2y)9ui#o0krf2%t3jwft558pm(*q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['mainb.us']
 
 
--- tools/reload.sh
+++ tools/reload.sh
@@ -1,2 +1,2 @@
-python3 ../manage.py collectstatic
+python3 manage.py collectstatic
 sudo systemctl restart gunicorn
