d7ad4578da6507f4691bc0d2fd1fedbe48a534e4
Jadon <jadonzufall@gmail.com>
2025-03-26 17:19:23 -0400

    Created authentication views

--- mainbus/views.py
+++ mainbus/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 
 def index(request) -> HttpResponse:
 	return HttpResponse("<h1>Hello World!</h1>")
+
+def signup(request) -> HttpResponse:
+    return HttpResponse("Invalid")
+
+def signin(request) -> HttpResponse:
+    return HttpResponse("Invalid")
+
+def signout(request) -> HttpResponse:
+    return HttpResponse("Invalid")
+
+
+
