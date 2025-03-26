70b5458a2c4c4c11d489428cb7fc276f0dc4dc00
Jadon <jadonzufall@gmail.com>
2025-03-26 17:33:20 -0400

    Removed views, something about them seemed to break the website?

--- mainbus/views.py
+++ mainbus/views.py
@@ -4,14 +4,3 @@ from django.http import HttpResponse
 def index(request) -> HttpResponse:
 	return HttpResponse("<h1>Hello World!</h1>")
 
-def signup(request) -> HttpResponse:
-    return HttpResponse("Invalid")
-
-def signin(request) -> HttpResponse:
-    return HttpResponse("Invalid")
-
-def signout(request) -> HttpResponse:
-    return HttpResponse("Invalid")
-
-
-
