d586c1c73b42b2a1c78982423df1d27dc2b3ab8b
JadonZufall <jadonzufall@gmail.com>
2025-04-05 06:01:27 -0400

    media player template, not complete just started

---
+++ mainbus/static/js/media.js
@@ -0,0 +1,19 @@
+
+
+document.querySelectorAll("button.media-button").forEach((button, index) => {
+	button.addEventListener("click", (event) => {
+		button.classList.toggle("is-active");
+	});
+
+	button.addEventListener("keydown", (event) => {
+		if (event.key === "Enter") {
+			button.classList.toggle("is-active");
+		}
+	});
+});
+
+document.querySelectorAll("button.media-button.play").forEach((button, index) => {
+	button.addEventListener("click", (event) => {
+		// Get media player and play it.
+	});
+});
