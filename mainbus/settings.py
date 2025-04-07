2ebadcecaacb64e6a86c7ba5fab87307eed07cbb
JadonZufall <jadonzufall@gmail.com>
2025-04-07 02:39:30 -0400

    changed dir for media

--- mainbus/settings.py
+++ mainbus/settings.py
@@ -9,7 +9,7 @@ DEBUG: bool = os.path.exists(".DEBUG")
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media_root")
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
