62b47cc2f47e52a06e002b531312f67dab2a697b
JadonZufall <jadonzufall@gmail.com>
2025-04-16 01:49:01 -0400

    js cookie lib

---
+++ apps/base/static/js/lib_cookies.js
@@ -0,0 +1,44 @@
+/* Library for parsing browser cookies. */
+const Cookies = {
+	set: (key, value) => {
+		
+	},
+	get: (key) => {
+
+	},
+	del: (key) => {
+
+	},
+	pop: (key) => {
+		let result = Cookies.get(key);
+		Cookies.del(key);
+		return result;
+	},
+	setdefault: (key, value) => {
+
+	},
+	iter: () => {
+		for (let cookie of document.cookie.split(";")) {
+			yield cookie;
+		}
+		return null;
+	},
+	keys: () => {
+		for (let cookie of document.cookie.split(";")) {
+			yield cookie.trim().split("=", 1)[0];
+		}
+	},
+	values: () => {
+		for (let cookie of document.cookie.split(";")) {
+			yield cookie.trim().split("=", 1)[1];
+		}
+	},
+	items: () => {
+		for (let cookie of document.cookie.split(";")) {
+			let cookie = cookie.trim.split("=", 1);
+			yield cookie[0], cookie[1];
+		}
+	}
+
+
+}
