d86e6e6e38f4348b611e00632ba39633c6abf96e
JadonZufall <jadonzufall@gmail.com>
2025-04-16 23:31:05 -0400

    custom components

---
+++ apps/base/static/js/lib_components.js
@@ -0,0 +1,8 @@
+class VSplit extends HTMLParagraphElement {
+	constructor() {
+	  super();
+	}
+	// Element functionality written in here
+  }
+
+  customElements.define("v-split", MyCustomElement);
