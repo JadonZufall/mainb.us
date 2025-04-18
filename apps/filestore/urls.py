8e0d1164810c24092a7680e8711e7e29a3cb48ca
JadonZufall <jadonzufall@gmail.com>
2025-04-17 23:19:56 -0400

    resolved type error, none does not work as placeholder...

--- apps/filestore/urls.py
+++ apps/filestore/urls.py
@@ -6,7 +6,7 @@ from .views.filestore_inspect_video import VideoView
 #f/
 urlpatterns = [
 	path("", ContentView.as_view(), name="home"), #todo - change view to correct view
-	path("delete/", None, name="delete_file"), #todo - change view to correct view
+	#path("delete/", None, name="delete_file"), #todo - change view to correct view
 	path("upload/", UploadView.as_view(), name="upload_file"),
 	path("image/<int:pk>/", VideoView.as_view(), name="inspect_image"), #todo - change view to correct view
 	path("other/<int:pk>/", VideoView.as_view(), name="inspect_other"), #todo - change view to correct view
