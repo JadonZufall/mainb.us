81509ae9a20a69f343cf041f678f5cb79a481103
JadonZufall <jadonzufall@gmail.com>
2025-04-17 23:10:00 -0400

    added todos that I will forget about later

--- apps/filestore/urls.py
+++ apps/filestore/urls.py
@@ -5,11 +5,11 @@ from .views.filestore_inspect_video import VideoView
 
 #f/
 urlpatterns = [
-	path("", ContentView.as_view(), name="home"),
-	path("delete/", None, name="delete_file"),
+	path("", ContentView.as_view(), name="home"), #todo - change view to correct view
+	path("delete/", None, name="delete_file"), #todo - change view to correct view
 	path("upload/", UploadView.as_view(), name="upload_file"),
-	path("image/<int:pk>/", VideoView.as_view(), name="inspect_image"), #todo - change view
-	path("other/<int:pk>/", VideoView.as_view(), name="inspect_other"),
-	path("audio/<int:pk>/", VideoView.as_view(), name="inspect_audio"), #todo - change view
+	path("image/<int:pk>/", VideoView.as_view(), name="inspect_image"), #todo - change view to correct view
+	path("other/<int:pk>/", VideoView.as_view(), name="inspect_other"), #todo - change view to correct view
+	path("audio/<int:pk>/", VideoView.as_view(), name="inspect_audio"), #todo - change view to correct view
 	path("video/<int:pk>/", VideoView.as_view(), name="inspect_video"),
 ]
