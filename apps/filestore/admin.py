1726d5abe220847a80c04ce82b704b509ce1a442
JadonZufall <jadonzufall@gmail.com>
2025-04-17 23:20:23 -0400

    temporarly commented out readonly fields

--- apps/filestore/admin.py
+++ apps/filestore/admin.py
@@ -7,7 +7,7 @@ import apps.filestore.models as models
 @admin.register(models.File)
 class FileAdmin(admin.ModelAdmin):
 	list_display = ["name", "file", "filetype", "views", "downloads", "author"]
-	readonly_fields = ["author", "views", "downloads", "filetype", "file" "date_created", "date_altered",]
+	#readonly_fields = ["author", "views", "downloads", "filetype", "file" "date_created", "date_altered",]
 
 	class Meta:
 		ordering = ["-date_created"]
