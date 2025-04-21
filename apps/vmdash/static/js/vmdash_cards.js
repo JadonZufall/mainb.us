abcaba9033d9076ee57bd71a3fb20885ad30d324
JadonZufall <jadonzufall@gmail.com>
2025-04-21 17:37:46 -0400

    update vmdash static content

--- apps/vmdash/static/js/vmdash_cards.js
+++ apps/vmdash/static/js/vmdash_cards.js
@@ -5,7 +5,38 @@ function vmdash_card_btn_delete(pk) {
 
 }
 
+function vmdash_card_btn_done(pk) {
+
+}
+
 function vmdash_card_btn_edit(pk) {
-	let element = document.getElementById(pk);
-	console.log(element);
+	let inputVIN = document.getElementById(`input-vin-${pk}`);
+	let spanVIN = document.getElementById(`span-vin-${pk}`);
+	inputVIN.removeAttribute("hidden");
+	spanVIN.setAttribute("hidden", true);
+	inputVIN.setAttribute("value", spanVIN.textContent);
+	if (inputVIN.getAttribute("value") === "None") {
+		inputVIN.setAttribute("value", "");
+	}
+	
+	let inputPlate = document.getElementById(`input-plate-${pk}`);
+	let spanPlate = document.getElementById(`span-plate-${pk}`);
+	inputPlate.removeAttribute("hidden");
+	spanPlate.setAttribute("hidden", true);
+	inputPlate.setAttribute("value", spanPlate.textContent);
+	if (inputPlate.getAttribute("value") === "None") {
+		inputVIN.setAttribute("value", "");
+	}
+
+	let inputMiles = document.getElementById(`input-miles-${pk}`);
+	let spanMiles = document.getElementById(`span-miles-${pk}`);
+	inputMiles.setAttribute("value", spanMiles.textContent);
+	inputMiles.removeAttribute("hidden");
+	spanMiles.setAttribute("hidden", true);
+
+	let editButton = document.getElementById(`button-edit-${pk}`);
+	editButton.setAttribute("hidden", true);
+
+	let doneButton = document.getElementById(`button-done-${pk}`);
+	doneButton.removeAttribute("hidden");
 }
